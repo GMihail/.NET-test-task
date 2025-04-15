6eea65538004ed99734a648e8492b260c6735d99
GMihail <118734496+GMihail@users.noreply.github.com>
2025-04-15 11:11:57 +0300

    Commit 5
    debug frontend

--- Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba09aa7d59fef6564e74f68594b8b259f4d9b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77cf9ec7eb6ad6b0ba99517a9a97c7a8435b5493")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
