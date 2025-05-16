092bd5897cf97a6550c0e52b60813d97284af97b
GMihail <118734496+GMihail@users.noreply.github.com>
2025-05-16 15:53:51 +0300

    Acrelis update

--- Shop/obj/Release/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Release/net8.0/Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0729bafdca612ea658953bfb067bd31ef5e616d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
